5adea800ebc505c64c1a96b2cedf49b6fae07f81
Juliot Nuñez <nunezjuliot@gmail.com>
2019-02-01 14:06:49 -0400

    fix index

--- app/Http/Controllers/MessengerController.php
+++ app/Http/Controllers/MessengerController.php
@@ -14,23 +14,24 @@ class MessengerController extends Controller
     	// i create a method for that.
     	$this->verifyAccess();
 
-        $user    = json_decode($this->getUser($id));
+        // $user    = json_decode($this->getUser($id));
     	$input   = json_decode(file_get_contents('php://input'), true);
-    	$id 	 = $input['entry'][0]['messaging'][0]['sender']['id'];
-    	$message = $input['entry'][0]['messaging'][0]['message']['text'];
+    	// $id 	 = $input['entry'][0]['messaging'][0]['sender']['id'];
+    	// $message = $input['entry'][0]['messaging'][0]['message']['text'];
 
-    	$response = [
-    		'recipient'		=>	['id'   => $id ],
-            'message'		=>	['text' => "Thanks for watching {$user->first_name} {$user->last_name}! :)"]
-    	];
+    	// $response = [
+    	// 	'recipient'		=>	['id'   => $id ],
+     //        'message'		=>	['text' => "Thanks for watching {$user->first_name} {$user->last_name}! :)"]
+    	// ];
 
-    	$this->sendMessage($response);
+    	// $this->sendMessage($response);
+        dd($input);
     }
 
     protected function sendMessage($response)
     {
     	// set our post
-    	$ch = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token=' . env('FACEBOOK_TOKEN'));
+    	$ch = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token=' . env('PAGE_ACCESS_TOKEN'));
     	curl_setopt($ch, CURLOPT_POST, 1);
     	curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));
     	curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
@@ -40,7 +41,7 @@ class MessengerController extends Controller
 
     protected function getUser($id = null)
     {
-        $url = "https://graph.facebook.com/v2.6/{$id}?fields=first_name,last_name,profile_pic&access_token=" . env('FACEBOOK_TOKEN');
+        $url = "https://graph.facebook.com/v2.6/{$id}?fields=first_name,last_name,profile_pic&access_token=" . env('PAGE_ACCESS_TOKEN');
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -54,7 +55,7 @@ class MessengerController extends Controller
     {
     	// FACEBOOK_MESSENGER_WEBHOOK_TOKEN is not exist yet.
     	// we can set that up in our .env file
-    	$local_token = env('MESSENGER_VERIFY_TOKEN');
+    	$local_token = env('FACEBOOK_MESSENGER_WEBHOOK_TOKEN');
     	$hub_verify_token = request('hub_verify_token');
 
     	// condition if our local token is equal to hub_verify_token
